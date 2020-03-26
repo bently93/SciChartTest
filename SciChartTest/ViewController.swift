369f4b8d57f035a591c2e74f2d33d91666b48075
Nikolay Leontev <leontevna@msk.bcs.ru>
2020-03-26 10:21:09 +0300

    Initial Commit

---
+++ SciChartTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SciChartTest
+//
+//  Created by n.leontev on 26.03.2020.
+//  Copyright © 2020 charttest. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
